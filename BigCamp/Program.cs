6934bc083e2221b70887641c5b66a22ead9a99ed
Hermar <127502706+HermarJR@users.noreply.github.com>
2023-05-02 18:59:43 +0800

    Add project files.

---
+++ BigCamp/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] bigCamp =
+        {
+            "Walmart", "State Grid Corporation of China", "Sinopec Group",
+            "China National Petroleum Corporation", "Toyota Motor",
+            "Volkswagen", "Royal Dutch Shell","Apple", "Berkshire Hathaway"
+            , "Exxon Mobil"
+        };
+
+        var index = 0;
+        while ((index < bigCamp.Length) && (bigCamp[index] != "Apple"))
+        {
+            index++;
+        }
+
+        Console.WriteLine("Apple is #{0} in the top 10 ", index + 1);
+
+
+        Console.ReadKey();
+    }
+}
